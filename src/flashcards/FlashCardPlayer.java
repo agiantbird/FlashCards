d1f80a5f0db7bd2562a89ecc0574ba7b218bc34a
Rich Hoyt <rhoyt10@gmail.com>
2022-05-09 12:41:09 -0400

    Build flash card player frame

--- src/flashcards/FlashCardPlayer.java
+++ src/flashcards/FlashCardPlayer.java
@@ -1,6 +1,9 @@
 package flashcards;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -11,11 +14,38 @@ public class FlashCardPlayer {
     private Iterator cardIterator;
     private FlashCard currentCard;
     private int currentCardIndex;
+    private JButton showAnswer;
     private JFrame frame;
     private boolean isShowAnswer;
 
     public FlashCardPlayer() {
         // Build UI
+        frame = new JFrame("Flash Card Player");
+        JPanel mainPanel = new JPanel();
+        Font mFont = new Font("Comic Sans MS", Font.BOLD, 21);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        display = new JTextArea(10, 20);
+        display.setFont(mFont);
+
+        JScrollPane qJScrollPane = new JScrollPane(display);
+        qJScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qJScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        showAnswer = new JButton("Show Answer");
+
+        mainPanel.add(qJScrollPane);
+        mainPanel.add(showAnswer);
+
+        showAnswer.addActionListener(new NextCardListener());
+
+        //Add to frame
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(640, 500);
+        frame.setVisible(true);
+
+
 
     }
 
@@ -26,5 +56,13 @@ public class FlashCardPlayer {
                 new FlashCardPlayer();
             }
         });
+    }
+
+    class NextCardListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
